ca7e0f2a3543399305fc9de06ffe76dcc8c02c7b
jainamshah3010 <jainamps18@gmail.com>
2026-06-19 13:49:02 +0530

    the sum of two numbers

--- ASSG2.java
+++ ASSG2.java
@@ -68,5 +68,17 @@ public class ASSG2{
         //     System.out.println("You are not eligible to vote.");
         // }
 
+        // 4. Define a program to find the sum of two numbers entered by the user.
+        // int a,b,sum;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a number: ");
+        // a = sc.nextInt();
+        // System.out.print("Enter another number: ");
+        // b = sc.nextInt();
+        // sum = a + b;
+        // System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+
+
+
     } 
 }
