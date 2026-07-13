0231040264b6eb8423301c79aa3481f09c024c5e
Jainam Shah <jainamps18@gmail.com>
2026-07-13 22:47:23 +0530

    completed 11 patterns

--- Patterns.java
+++ Patterns.java
@@ -136,11 +136,23 @@ public class Patterns {
 //         }
 
         //pattern 10
-        for(int i = 1;i <= 2*n-1; i++){
-            int stars = i;
-            if(i > n) stars = 2*n - i;
-            for(int j = 1;j <= stars; j++){
-                System.out.print("*");
+        // for(int i = 1;i <= 2*n-1; i++){
+        //     int stars = i;
+        //     if(i > n) stars = 2*n - i;
+        //     for(int j = 1;j <= stars; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        //pattern 11
+        int start = 1;
+        for(int i = 0;i < n;i++){
+            if(i % 2 == 0) start = 1;
+            else start = 0;
+            for(int j = 0;j <= i; j++){
+                System.out.print(start);
+                start = 1 - start;
             }
             System.out.println();
         }
