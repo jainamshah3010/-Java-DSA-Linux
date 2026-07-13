42ce26c56c260466309d94aad19f9825ae3d9d89
Jainam Shah <jainamps18@gmail.com>
2026-07-13 22:14:56 +0530

    completed 8 patterns

--- ASSG4.java
+++ ASSG4.java
@@ -1,35 +1,38 @@
 package ASSG;
 import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
+
 public class ASSG4 {
-     public static void main(String[] args) {
-        int[] nums = {};
-        int target = 50;
+//      public static void main(String[] args) {
+//         int[] nums = {};
+//         int target = 50;
 
-        int result = linearSearch(nums, target);
-        if (result != -1) {
-            System.out.println("Element found at index: " + result);
-        } else {
-            System.out.println("Element not found");
-        }
-     }
+//         int result = linearSearch(nums, target);
+//         if (result != -1) {
+//             System.out.println("Element found at index: " + result);
+//         } else {
+//             System.out.println("Element not found");
+//         }
+//      }
 
-     static int linearSearch(int[] arr, int target) {
-        //  if (arr.length == 0) {
-        //      return -1; // Return -1 if the array is empty
-        // }
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] == target) {
-                    return i; // Return the index if the target is found
-                }
-            }
-            return -1; // Return -1 if the target is not found
+//      static int linearSearch(int[] arr, int target) {
+//         //  if (arr.length == 0) {
+//         //      return -1; // Return -1 if the array is empty
+//         // }
+//             for (int i = 0; i < arr.length; i++) {
+//                 if (arr[i] == target) {
+//                     return i; // Return the index if the target is found
+//                 }
+//             }
+//             return -1; // Return -1 if the target is not found
 
-        } 
+//         } 
+
+
+
+
+
+
+
 }
 
     
--- Patterns.java
+++ Patterns.java
@@ -1,13 +1,83 @@
-package ASSG;
 
+package ASSG;
+import java.util.Scanner;
 public class Patterns {
     public static void main(String[] args) {
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        //pattern1
+        // ***
+        // ***
+        // ***
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < n; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+
+    //}
+
+   // pattern 2
+//    *
+//    **
+//    ***
+        // for(int i = 0;i < n; i++){
+        //     for(int j = 0;j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        //pattern 3
+        // for(int i = 1;i <= n; i++){
+        //     for(int j = 1;j <= i; j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+        //pattern 4
+        // for(int i = 1;i <= n; i++){
+        //     for(int j = 1;j <= i; j++){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println();
+        //}
+
+        //pattern 5
+        // for(int i = 1;i <= n; i++){
+        //     for(int j = 0;j < n-i+1; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        //pattern 6
+        // for(int i = 1;i <= n; i++){
+        //     for(int j = 1;j <= n-i+1; j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        //pattern 7
+       
+         for(int i = 0;i < n; i++){
+            //spaces
+            for(int j = 0;j < i; j++){
+                System.out.print(" ");
+            }
+            //stars
+
+            for(int j = 0;j < 2*n - (2*i + 1); j++){
                 System.out.print("*");
             }
+            //spaces
+            for(int j = 0;j < i; j++){
+                System.out.print(" ");
+            }
             System.out.println();
-
         }
     }
     
