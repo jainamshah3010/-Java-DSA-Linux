995b6d3da95be8873abec4075a70f8783dc24a42
Jainam Shah <jainamps18@gmail.com>
2026-07-14 23:16:38 +0530

    completed 13 patterns

--- Patterns.java
+++ Patterns.java
@@ -158,24 +158,36 @@ public class Patterns {
         // }
 
         //pattern 12
-        int space = 2*(n-1);
-        for(int i = 1;i <= n;i++){
-            //numbers
-            for(int j = 1;j <= i; j++){
-                System.out.print(j);
-            }
-            //spaces
-            for(int j = 1;j <= space; j++){
-                System.out.print(" ");
-            }
-            //numbers
-            for(int j = i;j >= 1; j--){
-                System.out.print(j);    
+        // int space = 2*(n-1);
+        // for(int i = 1;i <= n;i++){
+        //     //numbers
+        //     for(int j = 1;j <= i; j++){
+        //         System.out.print(j);
+        //     }
+        //     //spaces
+        //     for(int j = 1;j <= space; j++){
+        //         System.out.print(" ");
+        //     }
+        //     //numbers
+        //     for(int j = i;j >= 1; j--){
+        //         System.out.print(j);    
 
-        }
-        System.out.println();
-        space -= 2;
+        // }
+        // System.out.println();
+        // space -= 2;
            
+        // }
+
+        //pattern 13
+        int num = 1;
+        for(int i = 1;i <= n;i++){
+            for(int j = 1;j <= i; j++){
+                System.out.print(num + " ");
+                num = num + 1;
+
+            }
+            System.out.println();
         }
+        
     }
 }
