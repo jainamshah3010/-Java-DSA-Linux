db0299dd09a2a2ab237470c5b2af9361b878a2c6
Jainam Shah <jainamps18@gmail.com>
2026-07-15 22:41:13 +0530

    completed pattern 22

--- Patterns.java
+++ Patterns.java
@@ -315,6 +315,18 @@ public class Patterns {
         //     }
         //     System.out.println();
         // }
-                
+        
+        //pattern 22
+        //  for(int i = 0;i < 2*n-1; i++){
+        //     for(int j = 0;j < 2*n-1; j++){
+        //         int top = i;
+        //         int left = j;
+        //         int right = (2*n-2) - j;
+        //         int bottom = (2*n-2) - i;
+        //         int min = Math.min(Math.min(top, bottom), Math.min(left, right));
+        //         System.out.print(n - min + " ");
+        //     }
+        //     System.out.println();
+        // }
     }
 }
