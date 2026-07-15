5e74235b820354598bab80b4108247b6efb92771
Jainam Shah <jainamps18@gmail.com>
2026-07-15 21:31:51 +0530

    completed pattern 15

--- Patterns.java
+++ Patterns.java
@@ -196,5 +196,13 @@ public class Patterns {
         //     }
         //     System.out.println();
         // }
+
+        //pattern 15
+        for(int i = 0;i < n; i++){
+            for(char ch = 'A';ch <= 'A' + n - i - 1; ch++){
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
     }
 }
