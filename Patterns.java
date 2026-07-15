c99bf82d5193901a69135b9b0735175e828dbbe8
Jainam Shah <jainamps18@gmail.com>
2026-07-15 22:24:16 +0530

    completed pattern 19

--- Patterns.java
+++ Patterns.java
@@ -233,12 +233,52 @@ public class Patterns {
         //     }
         //     System.out.println();
         // }
+
+
         //pattern 18
+        // for(int i = 0;i < n; i++){
+        //     for(char ch =(char)('E' - i) ;ch <= 'E'; ch++){
+        //         System.out.print(ch);
+        //     }
+        //     System.out.println();
+        // }
+
+        //pattern 19
+        int inis = 0;
         for(int i = 0;i < n; i++){
-            for(char ch =(char)('E' - i) ;ch <= 'E'; ch++){
-                System.out.print(ch);
+        //stars
+            for(int j = 0;j < n-i; j++){
+                System.out.print("*");
             }
-            System.out.println();
+            //spaces
+            for(int j = 0;j < inis; j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j = 0;j < n-i; j++){
+                System.out.print("*");  
+
+        }
+        System.out.println();
+            inis += 2;
+        }
+        inis = 8;
+        for(int i = 1;i <= n; i++){
+            //stars
+            for(int j = 1;j <= i; j++){
+                System.out.print("*");
+            }
+            //spaces
+            for(int j = 0;j < inis; j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j = 1;j <= i; j++){
+                System.out.print("*");  
+
+        }
+        System.out.println();
+            inis -= 2;
         }
     }
 }
