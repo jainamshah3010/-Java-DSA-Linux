8426047c8c972e0899753b066ddec698917adc2d
Jainam Shah <jainamps18@gmail.com>
2026-07-15 22:33:25 +0530

    completed pattern 21

--- Patterns.java
+++ Patterns.java
@@ -282,25 +282,39 @@ public class Patterns {
         // }
 
         //pattern 20
-        int spaces = 2*n - 2;
-        for(int i = 1;i <= 2*n-1; i++){
-            int stars = i;
-            if(i > n) stars = 2*n - i;
-            //stars
-            for(int j = 1;j <= stars; j++){
-                System.out.print("*");
-            }
-            //spaces
-            for(int j = 1;j <= spaces; j++){
-                System.out.print(" ");
-            }
-            //stars
-            for(int j = 1;j <= stars; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-            if(i < n) spaces -= 2;
-            else spaces += 2;
-        }
+        // int spaces = 2*n - 2;
+        // for(int i = 1;i <= 2*n-1; i++){
+        //     int stars = i;
+        //     if(i > n) stars = 2*n - i;
+        //     //stars
+        //     for(int j = 1;j <= stars; j++){
+        //         System.out.print("*");
+        //     }
+        //     //spaces
+        //     for(int j = 1;j <= spaces; j++){
+        //         System.out.print(" ");
+        //     }
+        //     //stars
+        //     for(int j = 1;j <= stars; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        //     if(i < n) spaces -= 2;
+        //     else spaces += 2;
+        // }
+
+        //pattern 21
+        // for(int i = 0;i < n; i++){
+        //     for(int j = 0;j < n; j++){
+        //         if(i == 0 || j == 0 || i == n-1 || j == n-1){
+        //             System.out.print("*");
+        //         }
+        //         else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+                
     }
 }
