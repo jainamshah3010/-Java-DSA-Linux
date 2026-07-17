436cf14f207d480ede1473c29df043765fed3166
Jainam Shah <jainamps18@gmail.com>
2026-07-17 23:39:03 +0530

    updated

--- ASSG4.java
+++ ASSG4.java
@@ -14,18 +14,7 @@ public class ASSG4 {
 //         }
 //      }
 
-//      static int linearSearch(int[] arr, int target) {
-//         //  if (arr.length == 0) {
-//         //      return -1; // Return -1 if the array is empty
-//         // }
-//             for (int i = 0; i < arr.length; i++) {
-//                 if (arr[i] == target) {
-//                     return i; // Return the index if the target is found
-//                 }
-//             }
-//             return -1; // Return -1 if the target is not found
-
-//         } 
+//      
 
 
 
