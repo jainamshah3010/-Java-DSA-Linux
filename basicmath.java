79779c9df987a3bd82ddb4e8e6f05d14e6727519
Jainam Shah <jainamps18@gmail.com>
2026-07-18 11:18:16 +0530

    solved count digit problem

---
+++ basicmath.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class basicmath{
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int n = sc.nextInt();
+
+    //1 count digits
+    // count(n);
+    // }
+    
+    // static int count(int n){
+    //     int count = 0;
+    //     while(n > 0){
+           
+    //         count = count + 1;
+    //         n = n / 10;
+    //     }
+    //     System.out.println("Total digits: " + count);
+    //     return count;
+    }
+}
