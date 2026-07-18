aac55335a210cda94eaa1087232196d4f4efee00
Jainam Shah <jainamps18@gmail.com>
2026-07-18 11:42:36 +0530

    solved check a number is prime or not problem

--- basicmath.java
+++ basicmath.java
@@ -80,5 +80,20 @@ public class basicmath{
     //         }
     //     }
     //     System.out.println();
+
+    // To find out whether the given number is prime or not.
+    //     int n = 29;
+    //     boolean isPrime = true;
+    //     for (int i = 2; i <= n / 2; i++) {
+    //         if (n % i == 0) {
+    //             isPrime = false;
+    //             break;
+    //         }
+    //     }
+    //     if (isPrime) {
+    //         System.out.println(n + " is a prime number.");
+    //     } else {
+    //         System.out.println(n + " is not a prime number.");
+    //     }    
     }
 }
