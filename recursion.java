c985fa964b5e9b4300dd52bd04869572f49e56fa
Jainam Shah <jainamps18@gmail.com>
2026-07-20 18:32:12 +0530

    learned about recursion

--- recursion.java
+++ recursion.java
@@ -1,16 +1,22 @@
 public class recursion{
-    static int count = 0;
+    // static int count = 0;
     
-    public static void main(String[] args) {
-        print();
-    }
+    // public static void main(String[] args) {
+    //     print();
+    // }
      
-    static void print(){
-        if( count == 3){
-            return;
-        }
-        System.out.println(count);
-        count++;
-        print();
+    // static void print(){
+    //     if( count == 3){
+    //         return;
+    //     }
+    //     System.out.println(count);
+    //     count++;
+    //     print();
+    // }
+
+    public static void main(String[] args) {
+        //print name n times using recursion
+        printName(5);
+        
     }
 }
